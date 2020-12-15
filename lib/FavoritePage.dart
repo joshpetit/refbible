af507a7503b81d1c30de3535c06005c8bf72d761
joshu <joshuapetitma@yahoo.com>
2020-12-15 16:09:50 -0500

    beginnings filtering algorithm

--- lib/FavoritePage.dart
+++ lib/FavoritePage.dart
@@ -4,7 +4,6 @@ import 'package:fluttertoast/fluttertoast.dart';
 import 'RefVerse.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
-import 'package:flutter_typeahead/flutter_typeahead.dart';
 import 'dart:async';
 
 class FavoritesSection extends StatefulWidget {
@@ -14,6 +13,7 @@ class FavoritesSection extends StatefulWidget {
 
 class _FavoritesSectionState extends State<FavoritesSection> {
   final controller = TextEditingController();
+  final filtered = <RefVerse>[];
   final favorites = <RefVerse>[];
   Future<Database> database;
 
@@ -40,15 +40,7 @@ class _FavoritesSectionState extends State<FavoritesSection> {
     favorites.addAll(List.generate(verses.length, (i) {
       return RefVerse(verses[i]['reference'], verses[i]['text'], true);
     }));
-  }
-
-  Future<void> insertFavorite(RefVerse verse) async {
-    final Database db = await database;
-    await db.insert(
-      'favorite_verses',
-      verse.toMap(),
-      conflictAlgorithm: ConflictAlgorithm.replace,
-    );
+    filtered.addAll(favorites);
   }
 
   void _removeFavorite(RefVerse verse) {
@@ -67,6 +59,13 @@ class _FavoritesSectionState extends State<FavoritesSection> {
     );
   }
 
+  void filterList(String val) {
+    setState(() {
+      filtered.retainWhere(
+          (v) => v.toString().toLowerCase().contains(val.toLowerCase()));
+    });
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -76,18 +75,25 @@ class _FavoritesSectionState extends State<FavoritesSection> {
         alignment: Alignment.bottomCenter,
         child: Padding(
           padding: EdgeInsets.all(16),
-          child: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Container(
-                    height: MediaQuery.of(context).size.height * 0.70,
-                    child: _buildFavorites()),
-                Container(
-                  height: MediaQuery.of(context).size.height * 0.10,
-                  child: TextField(),
-                ),
-              ]),
+          child: SingleChildScrollView(
+            child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Container(
+                      height: MediaQuery.of(context).size.height * 0.70,
+                      child: _buildFavorites()),
+                  Container(
+                    height: MediaQuery.of(context).size.height * 0.10,
+                    child: TextField(
+                      controller: controller,
+                      onChanged: (String val) {
+                        filterList(val);
+                      },
+                    ),
+                  ),
+                ]),
+          ),
         ),
       ),
     );
